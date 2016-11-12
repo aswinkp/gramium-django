ccf3ef5152f848b80063cf85542974f8e88e86fd
Aswin Kumar KP <kp.aswinkumar@gmail.com>
2016-11-12 22:37:19 +0530

    model changes and mysql

--- gramium/settings.py
+++ gramium/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z8c70iy-_f$@&a8#c-cl(&c^(mcvsbem530lv1+cq*a(3hm^6v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +75,22 @@ WSGI_APPLICATION = 'gramium.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gramium',
+        'USER': 'gramium',
+        'PASSWORD': 'Demo@1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
